fb7f12eb5e1255aa131ad05ccb2d20bcd8ecdb3d
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-12-02 09:51:02 +0530

    loops

--- loops.js
+++ loops.js
@@ -1,10 +1,37 @@
 
-var arr=[];
-var i=0;
+// var arr=[];
+// var i=0;
 
-do{
-    arr.push(i);
-    i++;
-}while(i<5);
+// do{
+//     arr.push(i);
+//     i++;
+// }while(i<5);
 
-console.log(arr);
+// console.log(arr);
+
+let data = {
+    email: 'kunal@email.com',
+    profile: 'Backend developer',
+    exp: '1 yr'
+}
+
+// Looping
+// Object.keys(data).map(el=>{
+//     console.log(el);
+// })
+
+// Object.keys(data).forEach(el=>{
+//     console.log(el);
+// })
+
+let arr=[1,2,3,4,5]
+
+// -----------For Loop----------
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+//---------forEach Loop-----------
+// arr.forEach(el=>{
+//     console.log(el);
+// })
