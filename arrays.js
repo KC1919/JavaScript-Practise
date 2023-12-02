32f4c0ccfcb155a763374610cc72de1944470861
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-12-02 09:44:58 +0530

    object and array

--- arrays.js
+++ arrays.js
@@ -1,25 +1,48 @@
-var array=[1,2,3];
+// Declaration ways
+// 1) 
+let array = [1, 2, 3];
 
-// array.push("Kunal");
+// 2)
+let arr = new Array(1, 2, 3, 4, "Kunal")
+// console.log(arr);
+
+//3 
+let arr1 = ['apple', 'banana', 'grapes', 'papaya']
+// console.log(arr1);
+
 
 //adds an element to the back of the array
-array.push([4,5,6,"kunal"]);
+array.push([4, 5, 6, "kunal"]);
+
+// Add two or more arrays together
+let a1 = [1, 2, 3]
+let a2 = [4, 5, 6]
+
+// Method 1
+let a3 = a1.concat(a2);
+// console.log(a3);
+
+// Method 2
+let a4 = [...a1, ...a2]
+console.log(a4);
 
 // console.log(array);
 // console.log(array[3][3]);  
 
+// Removes the last element of the array
 // array.pop();
 
 array[0]++;
 
-//removes first eleemnt of the array
-var remData=array.shift();
-
+//removes first elemnt of the array
+var remData = array.shift();
 // console.log(remData);
 
 // console.log(array);
 
-//adds an element to the front of the array
-array.unshift(["Ayush",24]);
+//adds an element in front of the array
+array.unshift("Ayush");
+// console.log(array);
 
-console.log(array);
+arr1.unshift('orange');
+// console.log(arr1);
--- objects.js
+++ objects.js
@@ -1,20 +1,65 @@
-var data={
-    "name":"Kunal",
+var data = {
+    "name": "Kunal",
     "age": 22,
     "sex": 'M',
-    "friends":["Adi","Mukku","Goro"]
+    "friends": ["Adi", "Mukku", "Goro"]
 };
 
-//this method makes the object immutable
+//----this method makes the object immutable-----
 Object.freeze(data);
 
-// ways to access object attributes
-console.log(data.friends);  //using dot operator
-console.log(data["name"]);  //using name of the attribute
-console.log(data["sex"]);
+//------ Ways to access object attributes----------
+// using dot operator
+// console.log(data.friends);  
+// using name of the attribute
+// console.log(data["name"]);  
+
 
 //delete keyword, deletes the passed object key 
 delete data["age"];
 delete data.sex;
 
-console.log(data);
+// console.log(data);
+
+//------- Object destructuring------------
+
+let { name } = data
+// console.log(name);
+
+// we can give custom name to properties
+let { name: n } = data
+//gives the same O/P as line 27
+// console.log(n);
+
+let data2 = {
+    email: 'kunal@email.com',
+    profile: 'Backend developer',
+    exp: '1 yr'
+}
+
+//----------- Concatenating the two objects------------
+let data3 = { ...data, ...data2 }
+// console.log(data3);
+
+let data4=Object.assign({},data,data2) // gives the same output as line 40
+// console.log(data4);
+
+// -------Loop through objects keys and values or both-------------
+
+// returns an array of object keys
+// console.log(Object.keys(data2));
+
+// returns an array of object values
+// console.log(Object.values(data2));
+
+// returns an 2d array with key and value pair as an each array at each index
+// console.log(Object.entries(data2));
+
+// Looping
+Object.keys(data2).map(el=>{
+    console.log(data2[el]);
+})
+
+Object.keys(data2).forEach(el=>{
+    console.log(data2[el]);
+})
